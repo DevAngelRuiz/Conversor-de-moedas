aa2e2eaba0406c1c47bb03c9b0c1ec12a6ebf57d
DevAngelRuiz <101833891+DevAngelRuiz@users.noreply.github.com>
2022-05-12 11:35:35 -0300

    add bitcoin currency

--- scripts.js
+++ scripts.js
@@ -4,11 +4,12 @@ let select = document.getElementById("select-moedas")
 
 
 async function converterMoedas() {
-    let moedas = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL').then(function (resposta) {
+    let moedas = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL').then(function (resposta) {
         return resposta.json()
     })
 let dolar = moedas.USDBRL.high
 let euro = moedas.EURBRL.high
+let bitcoin = moedas.BTCBRL.high
 
     console.log(moedas)
    
@@ -20,20 +21,23 @@ let euro = moedas.EURBRL.high
         let valorEmDolares = inputValorEmReais / dolar
         inputMoedas.innerHTML = valorEmDolares.toLocaleString('en-us', { style: 'currency', currency: 'USD' })
 
-
     }
     if (select.value === "€ Euro") {
         let valorEmEuros = inputValorEmReais / euro
         inputMoedas.innerHTML = valorEmEuros.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
 
+    }
 
-
-
+    if (select.value === "Bitcoin") {
+        let valorEmBitcoin = (inputValorEmReais / bitcoin) /1000
+        inputMoedas.innerHTML = valorEmBitcoin.toLocaleString('en-IN', { style: 'currency', currency: 'BTC' })
 
     }
 
+
     inputReal.innerHTML = inputValorEmReais.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
 }
+
 
 // essa função troca o nome e bandeira das moedas 
 function trocaDeMoedas() {
@@ -48,6 +52,10 @@ function trocaDeMoedas() {
     if (select.value === "€ Euro") {
         textoMoedas.innerHTML = "Euro"
         bandeiraMoedas.src = "./img/euro.png"
+    }
+    if (select.value === "Bitcoin") {
+        textoMoedas.innerHTML = "Bitcoin"
+        bandeiraMoedas.src ="./img/btc.png"
     }
     converterMoedas()
 }
